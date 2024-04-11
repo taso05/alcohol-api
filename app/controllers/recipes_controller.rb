98dbee63bec9ce1b4fef733660429216058708e0
三原綾華 <>
2024-04-11 22:06:28 +0900

    レシピの追加編集削除

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -1,6 +1,58 @@
 class RecipesController < ApplicationController
-    def index
-      recipes = Recipe.all
-      render json: recipes
+  def index
+    recipes = Recipe.all
+    render json: recipes
+  end
+
+  # レシピの新規作成
+  def create
+    recipe = Recipe.new({
+                          title: params[:title], # タイトル
+                          content: params[:content], # 説明文
+                          percentage: params[:percentage], # お酒の度数
+                          sweetness: params[:sweetness], # お酒の甘さ
+                          image: params[:image], # 画像
+                          user_id: params[:user_id], # ユーザーID
+                        })
+
+    if recipe.save
+      render json: recipe, status: :created
+    else
+      render json: recipe.errors, status: :unprocessable_entity
     end
   end
+
+  # レシピの編集
+  def update
+    recipe = Recipe.find(params[:id])
+    if recipe.update(
+      title: params[:title],
+      content: params[:content],
+      percentage: params[:percentage],
+      sweetness: params[:sweetness],
+      image: params[:image],
+      user_id: params[:user_id],
+    )
+      render json: recipe, status: :ok
+    else
+      render json: recipe.errors, status: :unprocessable_entity
+    end
+  end
+
+  # private
+
+  # ストロングパラメータ
+  # def recipe_params
+  #   params.require(:recipe).permit(:title, :description, :ingredients, :instructions)
+  # end
+
+  # レシピの削除
+  def destroy
+    recipe = Recipe.find(params[:id])
+    if recipe.destroy
+      render json: { message: '削除しました。' }, status: :ok
+    else
+      render json: { message: '削除に失敗しました' }, status: :unprocessable_entity
+    end
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -4,8 +4,9 @@ Rails.application.routes.draw do
   # Defines the root path route ("/")
   # root "articles#index"
 
-
   get '/users', to: 'users#index'
   get '/recipes', to: 'recipes#index'
-
+  post '/recipes', to: 'recipes#create'
+  delete '/recipes/:id', to: 'recipes#destroy'
+  put '/recipes/:id', to: 'recipes#update'
 end
